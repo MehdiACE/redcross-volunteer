4372b2793b1326ff40a9beba18ebf31f09d45c44
MehdiACE <mehdi.mahmoudi@aceenergie.com>
2026-01-25 22:03:26 +0100

    disable ssr - fix build

--- RedCrossManager.Client/src/app/app.config.server.ts
+++ RedCrossManager.Client/src/app/app.config.server.ts
@@ -1,6 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
-import { appConfig } from './app.config';
+import { appBaseConfig } from './app.config.base';
 
 const serverConfig: ApplicationConfig = {
   providers: [
@@ -8,4 +8,4 @@ const serverConfig: ApplicationConfig = {
   ]
 };
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export const config = mergeApplicationConfig(appBaseConfig, serverConfig);
--- RedCrossManager.Client/src/app/app.config.ts
+++ RedCrossManager.Client/src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { appBaseConfig } from './app.config.base';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    ...(appBaseConfig.providers ?? []),
+    provideClientHydration()
+  ]
 };
