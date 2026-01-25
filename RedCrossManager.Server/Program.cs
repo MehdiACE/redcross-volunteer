711d099caa415916991995dd6fee868b1491e8c0
MehdiACE <mehdi.mahmoudi@aceenergie.com>
2026-01-25 22:29:52 +0100

    add swagger to backend

--- RedCrossManager.Server/Program.cs
+++ RedCrossManager.Server/Program.cs
@@ -17,7 +17,8 @@ builder.Host.UseSerilog((ctx, lc) => lc.ReadFrom.Configuration(ctx.Configuration
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Database
 builder.Services.AddDbContext<RedCrossDbContext>(options =>
@@ -77,7 +78,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 // Health endpoints
