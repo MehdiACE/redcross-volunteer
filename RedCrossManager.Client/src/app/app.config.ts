951aa89d4d1dfc6a1290ed681b1363a17b7aa469
MehdiACE <mehdi.mahmoudi@aceenergie.com>
2026-01-25 23:23:31 +0100

    feat: Update Angular configuration and styles for improved UI and asset management

--- RedCrossManager.Client/src/app/app.config.ts
+++ RedCrossManager.Client/src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { appBaseConfig } from './app.config.base';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     ...(appBaseConfig.providers ?? []),
-    provideClientHydration()
+    provideClientHydration(), provideAnimationsAsync()
   ]
 };
