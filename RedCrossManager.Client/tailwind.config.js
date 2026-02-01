e9870484594f6ce90a1b728dcdd283c5f46fd205
MehdiACE <mehdi.mahmoudi@aceenergie.com>
2026-02-01 21:06:03 +0100

    feat: enhance UI with dark mode support; update background colors across components

--- RedCrossManager.Client/tailwind.config.js
+++ RedCrossManager.Client/tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/**/*.{html,ts}'
   ],
   theme: {
     extend: {
       colors: {
-        'app-bg': '#F5F5F0'
+        'app-bg': '#F5F5F0',
+        'app-bg-dark': '#191a1f',
+        'text-dark': '#3f3f46'
       }
     },
   },
